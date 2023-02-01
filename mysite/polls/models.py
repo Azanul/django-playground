8f0eaf7828ab8fa95fe1ac1dafbc39dd77e310fe
Azanul <azanulhaque@gmail.com>
2023-02-01 17:49:42 +0000

    custom lookup ADD

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,18 @@
+from django.db.models import Field, Lookup
 from django.db import models
+
+
+@Field.register_lookup
+class LengthWithinLookup(Lookup):
+    lookup_name = 'length_within'
+
+    def as_sql(self, compiler, connection):
+        lhs, lhs_params = self.process_lhs(compiler, connection)
+        rhs, rhs_params = self.process_rhs(compiler, connection)
+        print(f"lhs: {lhs}, {lhs_params}")
+        print(f"rhs: {rhs}, {rhs_params}")
+        lower_limit, upper_limit = rhs_params[0][1:-1].split(", ")
+        return f"LENGTH({lhs}) BETWEEN {lower_limit} AND {upper_limit}", []
 
 
 class Question(models.Model):
