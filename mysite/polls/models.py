a3c7268acee7ce79127b7fa4c23e2c3d76698b33
Azanul <azanulhaque@gmail.com>
2023-02-02 06:53:11 +0000

    NotQuestion ADD

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -24,3 +24,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class NotQuestion(models.Model):
+    question_text = models.CharField(max_length=200)
+    due_date = models.DateTimeField('due date')
+    category = models.CharField(max_length=100)
