3b0842f3f914ed39bfb67c9c48be0c72ee1d4796
Azanul <azanulhaque@gmail.com>
2023-02-02 06:57:34 +0000

    RawSQL view ADD

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from django.views import View
 
+from .models import Question
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+class RawSQL(View):
+    def get(self, *args, **kwargs):
+        field_map = {'due_date': 'pub_date'}
+        notquestions = Question.objects.raw('SELECT * FROM polls_notquestion', translations=field_map)
+        results = []
+        for nq in notquestions:
+            results += (nq.category, nq.question_text, str(nq.pub_date))
+            results += "-----"
+        return HttpResponse(", ".join(results))
